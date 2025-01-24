85598078ced6ac3f7e9194c96e10ae9ca223b519
Ishwor <ishwortimalsina39@gmail.com>
2025-01-24 22:14:12 +0545

    react quiz

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,17 +1,18 @@
 import { useEffect, useReducer } from 'react';
-import Header from './Header';
-import Main from './main';
-import Loader from './Loader';
-import Error from './Error';
-import StartScreen from './StartScreen';
-import Question from './Question';
+import Header from './components/Header';
+import Main from './components/main';
+import Loader from './components/Loader';
+import Error from './components/Error';
+import StartScreen from './components/StartScreen';
+import Question from './components/Question';
 
 const initialState = {
   questions: [],
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
-
 
 function reducer(state, action) {
   switch (action.type) {
@@ -21,22 +22,36 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
-      
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
     default:
       throw new Error('Action unknown');
   }
 }
 
-
 export default function App() {
-  const [{ questions, status , index}, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
   useEffect(function () {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
-      .then((data) => dispatch({ type: 'dataRecieved', payload: data }))
+      .then((data) => {
+        dispatch({ type: 'dataRecieved', payload: data });
+      })
       .catch((err) => dispatch({ type: 'dataFailed' }));
   }, []);
 
@@ -50,7 +65,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]}/>}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
