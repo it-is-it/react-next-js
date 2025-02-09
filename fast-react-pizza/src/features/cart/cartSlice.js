edd30ba79aa487a4858e62ec7912e7d945226860
Ishwor <ishwortimalsina39@gmail.com>
2025-02-10 00:16:42 +0545

    added new features

--- fast-react-pizza/src/features/cart/cartSlice.js
+++ fast-react-pizza/src/features/cart/cartSlice.js
@@ -2,16 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   cart: [],
-
-  // cart: [
-  //   {
-  //     pizzaId: 12,
-  //     name: 'Mreterid',
-  //     quantity: 2,
-  //     unitPrice: 16,
-  //     totalPrice: 32,
-  //   },
-  // ],
 };
 
 const cartSlice = createSlice({
