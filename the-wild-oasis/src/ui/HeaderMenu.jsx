78f1d68c0563945df4a8d61fccf21c5938add482
Ishwor <ishwortimalsina39@gmail.com>
2025-05-11 23:24:11 +0545

    new file

---
+++ the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import ButtonIcon from './ButtonIcon';
+import Logout from '../features/authentication/Logout';
+import { HiOutlineUser } from 'react-icons/hi2';
+import styled from 'styled-components';
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
