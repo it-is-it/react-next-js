803890ec59045421cf5224bed0abf033830f04d8
Ishwor <ishwortimalsina39@gmail.com>
2025-05-14 23:15:29 +0545

    new file

---
+++ the-wild-oasis/src/context/DarkModeContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect } from 'react';
+import { useLocalStorageState } from '../hooks/useLocalStorageState';
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.classList.add('dark-mode');
+      root.classList.remove('light-mode');
+    } else {
+      root.classList.remove('dark-mode');
+      root.classList.add('light-mode');
+    }
+  }, [isDarkMode]);
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined) {
+    throw new Error('useDarkMode must be used within a DarkModeProvider');
+  }
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
